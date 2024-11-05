d8d38bbc475d1e57fa2e4799667eadefbe4b06f6
hani hasn <hanihasna85@gmail.com>
2024-11-05 11:55:47 +0700

    update migration&model

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,12 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'username',
         'password',
+        'role',
+        'gender',
+        'email',
+        'city',
     ];
 
     /**
